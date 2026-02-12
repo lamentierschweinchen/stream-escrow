2e6be99f0c032e2a7437c5bc8ad919be8699ef3d
Lukas <ls@MacBook-Pro-2.local>
2026-02-12 13:28:23 -0600

    Wire deployed contract into frontend and CLI defaults

--- cli/config.py
+++ cli/config.py
@@ -2,8 +2,8 @@ PROXY_URL = "https://api.claws.network"
 CHAIN_ID = "C"
 GAS_PRICE = 20_000_000_000_000
 
-# Update after deployment.
-CONTRACT_ADDRESS = ""
+# Deployed on chain C (2026-02-12)
+CONTRACT_ADDRESS = "claw1qqqqqqqqqqqqqpgqnlglzrzv598wqgx6zlhjr6rky2k694jtkgcqdfq0ge"
 
 BYTECODE_PATH = "./output/stream-escrow.wasm"
 
